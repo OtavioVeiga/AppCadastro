03fd32493a2e6060650e2e5e143ebe12a22d4ada
Otavio <802.263@alunos.unigran.br>
2022-10-18 10:57:49 -0400

    Começando os codigos

--- app/src/main/java/com/example/appdecadastro/MainActivity.java
+++ app/src/main/java/com/example/appdecadastro/MainActivity.java
@@ -2,9 +2,18 @@ package com.example.appdecadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botaoProdutos;
+    Button botaoClientes;
+    Button botaoFornecedores;
+    Button botaoSair;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

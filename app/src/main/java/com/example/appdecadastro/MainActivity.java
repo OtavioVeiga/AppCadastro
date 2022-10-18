024931e0d734207f3e2490c529bca13cdfa5db42
Otavio <802.263@alunos.unigran.br>
2022-10-18 11:11:50 -0400

    Codigo Primeira tela

--- app/src/main/java/com/example/appdecadastro/MainActivity.java
+++ app/src/main/java/com/example/appdecadastro/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +20,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoClientes = findViewById(R.id.botaoClientes);
+        botaoSair = findViewById(R.id.botaoSair);
+        botaoFornecedores = findViewById(R.id.botaoFornecedores);
+        botaoProdutos = findViewById(R.id.botaoProdutos);
+
+        acoesComponentes();
+    }
+
+    private void acoesComponentes() {
+        botaoProdutos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("operacao", 0);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoFornecedores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("operacao", 1);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("operacao", 2);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
     }
 }
